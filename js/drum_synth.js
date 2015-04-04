9e98b71c8ed561c691aed63afdd6bbbe4b56ada2
Devin Dominguez <dominguez.devin@gmail.com>
2015-04-04 08:14:17 -0700

    added highhat

--- js/drum_synth.js
+++ js/drum_synth.js
@@ -66,20 +66,33 @@ var settings = {
 		freqEnd: 50
 	},
 	closedHat: {
-
+		decay: 25,
+		sustain: 15,
+		release: 20,
+		sustainLevel: .125,
+		freq: 4000,
+		Q: 20,
+		sineLevel: 0
 	},
 	openHat: {
+		decay: 150,
+		sustain: 250,
+		release: 750,
+		sustainLevel: .25,
+		freq: 4000,
+		Q: 20,
+		sineLevel: 0
 
 	},
 	percussion :{
-		
+
 	}
 };
 
 //----------------------------------------------------
 //NOISE DRUM
 //----------------------------------------------------
-function createNoiseDrum() {
+function NoiseDrum() {
 	var filter = audio.createBiquadFilter();
 	filter.type = "bandpass";
 
@@ -111,39 +124,47 @@ function createNoiseDrum() {
 	sine.connect(sineAmp);
 	sineAmp.connect(amp);
 
-	amp.connect(out);
+	//amp.connect(out);
 	
-	return function(args, time, velocity) {
-		//ARGS = decay, sustain, release, sustainLevel, freq, Q, sineLevel
-		var now = time || audio.currentTime;
-		var level = velocity * velocity || .5625;
-		var decay = args.decay / 1000;
-		var sustain = args.sustain / 1000;
-		var release = args.release / 1000;
-		var sustainLevel = args.sustainLevel;
-		var sineLevel = args.sineLevel;
-		var freq = args.freq;
-		var Q = args.Q;
+	return {
+		connect: function(target) {
+			amp.connect(target);
+		},
+		disconnect: function(target) {
+			amp.disconnect(target);
+		},
+		trigger: function(args, time, velocity) {
+			//ARGS = decay, sustain, release, sustainLevel, freq, Q, sineLevel
+			var now = time || audio.currentTime;
+			var level = velocity * velocity || .5625;
+			var decay = args.decay / 1000;
+			var sustain = args.sustain / 1000;
+			var release = args.release / 1000;
+			var sustainLevel = args.sustainLevel;
+			var sineLevel = args.sineLevel;
+			var freq = args.freq;
+			var Q = args.Q;
 
-		filter.frequency.setValueAtTime(freq, now);
-		filter.Q.setValueAtTime(Q, now);
+			filter.frequency.setValueAtTime(freq, now);
+			filter.Q.setValueAtTime(Q, now);
 
-		sine.frequency.setValueAtTime(freq / 4, now);
-		sineAmp.gain.setValueAtTime(sineLevel, now);
+			sine.frequency.setValueAtTime(freq / 4, now);
+			sineAmp.gain.setValueAtTime(sineLevel, now);
 
-		env.gain.cancelScheduledValues(now);
-		env.gain.linearRampToValueAtTime(0, now + .005);
-		env.gain.linearRampToValueAtTime(1, now + .01);
-		env.gain.linearRampToValueAtTime(sustainLevel, now + .01 + decay);
-		env.gain.setValueAtTime(sustainLevel, now + .01 + decay + sustain);
-		env.gain.linearRampToValueAtTime(0, now + .01 + decay + sustain + release);
+			env.gain.cancelScheduledValues(now);
+			env.gain.linearRampToValueAtTime(0, now + .005);
+			env.gain.linearRampToValueAtTime(1, now + .01);
+			env.gain.linearRampToValueAtTime(sustainLevel, now + .01 + decay);
+			env.gain.setValueAtTime(sustainLevel, now + .01 + decay + sustain);
+			env.gain.linearRampToValueAtTime(0, now + .01 + decay + sustain + release);
+		}
 	};
 }
 
 //----------------------------------------------------
 //SINE DRUM
 //----------------------------------------------------
-function createSineDrum() {
+function SineDrum() {
 	var amp = audio.createGain();
 	amp.gain.value = 0;
 
@@ -165,31 +186,47 @@ function createSineDrum() {
 	envFilter.connect(amp.gain);
 
 	sine.connect(amp);
-	amp.connect(out);
+	//amp.connect(out);
 	
-	return function(args, time, velocity) {
-		//ARGS = decay, sustain, release, sustainLevel, freqStart, freqEnd
-		var now = time || audio.currentTime;
-		var level = velocity * velocity || .5625;
-		var decay = args.decay / 1000;
-		var sustain = args.sustain / 1000;
-		var release = args.release / 1000;
-		var sustainLevel = args.sustainLevel * level;
-		var freqStart = args.freqStart;
-		var freqEnd = args.freqEnd;
+	return {
+		connect: function(target) {
+			amp.connect(target);
+		},
+		disconnect: function(target) {
+			amp.disconnect(target);
+		},
+		trigger: function(args, time, velocity) {
+			//ARGS = decay, sustain, release, sustainLevel, freqStart, freqEnd
+			var now = time || audio.currentTime;
+			var level = velocity * velocity || .5625;
+			var decay = args.decay / 1000;
+			var sustain = args.sustain / 1000;
+			var release = args.release / 1000;
+			var sustainLevel = args.sustainLevel * level;
+			var freqStart = args.freqStart;
+			var freqEnd = args.freqEnd;
 
-		sine.frequency.cancelScheduledValues(now + .009);
-		sine.frequency.setValueAtTime(freqStart, now + .01);
-		sine.frequency.exponentialRampToValueAtTime(freqEnd, now + .01 + decay + sustain + release);
+			sine.frequency.cancelScheduledValues(now + .009);
+			sine.frequency.setValueAtTime(freqStart, now + .01);
+			sine.frequency.exponentialRampToValueAtTime(freqEnd, now + .01 + decay + sustain + release);
 
-		env.gain.cancelScheduledValues(now);
-		env.gain.linearRampToValueAtTime(0, now + .009);
-		env.gain.linearRampToValueAtTime(level, now +.01);
-		env.gain.linearRampToValueAtTime(sustainLevel, now + .01 + decay);
-		env.gain.setValueAtTime(sustainLevel, now + .01 + decay + sustain);
-		env.gain.linearRampToValueAtTime(0, now + .01 + decay + sustain + release);
+			env.gain.cancelScheduledValues(now);
+			env.gain.linearRampToValueAtTime(0, now + .009);
+			env.gain.linearRampToValueAtTime(level, now +.01);
+			env.gain.linearRampToValueAtTime(sustainLevel, now + .01 + decay);
+			env.gain.setValueAtTime(sustainLevel, now + .01 + decay + sustain);
+			env.gain.linearRampToValueAtTime(0, now + .01 + decay + sustain + release);
+		}
 	};
 }
 
-var snare = createNoiseDrum();
-var kick = createSineDrum();
+//----------------------------------------------------
+//SETUP CHANNELS
+//----------------------------------------------------
+
+var snare = new NoiseDrum();
+snare.connect(out);
+var kick = new SineDrum();
+kick.connect(out);
+var highHat = new NoiseDrum();
+highHat.connect(out);
